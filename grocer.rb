5dfb2da6ad784f0c419128e1e75c545792631e48
Connor Meth <connormeth27@gmail.com>
2019-09-14 16:26:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,16 +14,16 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon_hash|
-    coupons.each do |coupon|
-    name = coupon[:item]
-    if cart[name] && cart[name][:count] >= coupon[:num]
+    coupons.each do |coupons|
+    name = coupons[:item]
+    if cart[name] && cart[name][:count] >= coupons[:num]
       if cart["#{name} W/COUPON"]
         cart["#{name} W/COUPON"][:count] += 1
       else
-        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost]}
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupons[:cost]}
         cart["#{name} W/COUPON"][:clearance] = cart[name][:clearance]
       end
-      cart[name][:count] -= coupon[:num]
+      cart[name][:count] -= coupons[:num]
     end
   end
   cart
