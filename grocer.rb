69877c153a33d8285a209d90431e5dc6537e3b70
Connor Meth <connormeth27@gmail.com>
2019-09-14 15:43:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,8 +2,8 @@ def consolidate_cart(cart)
   cart_hash = {}
   cart.each do |item|
     item.each do |name, price_hash|
-    if cart_hash[item_name].nil?
-      cart_hash[item_name][:count] += 1 
+    if cart_hash[item].nil?
+      cart_hash= price_hash.merge({:count => 1}) 
     else cart_hash[item_name] = item[item_name]
       cart_hash[item_name][:count] = 1
     end
