5092aae6007a03e26c4bdcdfa9b2396eb48f64f7
Connor Meth <connormeth27@gmail.com>
2019-09-14 15:42:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def consolidate_cart(cart)
   cart_hash = {}
   cart.each do |item|
-    item.each do 
+    item.each do |name, price_hash|
     if cart_hash[item_name].nil?
       cart_hash[item_name][:count] += 1 
     else cart_hash[item_name] = item[item_name]
