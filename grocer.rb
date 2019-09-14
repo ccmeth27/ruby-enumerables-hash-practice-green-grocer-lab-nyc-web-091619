0001bdc6a69ed0961d982968ffbe8d62f6963774
Connor Meth <connormeth27@gmail.com>
2019-09-14 15:53:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,8 @@ end
 def apply_coupons(cart, coupons)
   cart_hash = cart 
   coupons.each do |coupons_hash|
-    item = coupons_hash[item]
+    item = coupons_hash[:item]
+    
 end
 
 def apply_clearance(cart)
